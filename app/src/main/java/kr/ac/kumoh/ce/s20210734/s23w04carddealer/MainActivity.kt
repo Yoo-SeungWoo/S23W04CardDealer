11d5681c7d730b47746681fe06728edec0408fc1
USER <yoosw0329@naver.com>
2023-10-16 14:16:03 +0900

    .

--- app/src/main/java/kr/ac/kumoh/ce/s20210734/s23w04carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20210734/s23w04carddealer/MainActivity.kt
@@ -14,5 +14,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(main.root)
 
         main.card1.setImageResource(R.drawable.c_2_of_hearts)
+
+        val res = resources.getIdentifier(
+            //TODO:하드 코딩 제거할 것
+            getCardName(37),
+            "drawable",
+            packageName
+        )
+
+        main.card1.setImageResource(res)
+    }
+
+    private fun getCardName(c: Int) :String {
+        val shape = when (c / 13) {
+            0 -> "spades"
+            1 -> "diamonds"
+            2 -> "hearts"
+            3 -> "clubs"
+            else -> "error"
+        }
+
+        val number = when(c % 13) {
+            0 -> "ace"
+            in 1..9 -> (c % 13 + 1).toString()
+            10 -> "jack"
+            11 -> "queen"
+            12 -> "king"
+            else -> "error"
+        }
+        return if(number in arrayOf("jack", "queen","king"))
+            "c_{$number}_of_{$shape}2"
+        else
+            "c_{$number}_of_{$shape}"
     }
 }
